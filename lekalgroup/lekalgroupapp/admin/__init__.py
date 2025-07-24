958142d7b8f0ff6529147b3920d3064d568be9f0
Əbil Abilli <ebilebilli3@gmail.com>
2025-07-24 18:35:38 +0400

    Partner model created

--- lekalgroup/lekalgroupapp/admin/__init__.py
+++ lekalgroup/lekalgroupapp/admin/__init__.py
@@ -1,3 +1,4 @@
 from .category_admin import *
 from .product_admin import *
 from .background_image_admin import *
+from .partner_admin import *
---
+++ lekalgroup/lekalgroupapp/models/partner.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Partner(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Ad'
+    )
+    logo = models.ImageField(
+        upload_to='logo_images/',
+        verbose_name='Əməkdaş logo şəkli',
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Əməkdaş'
+        verbose_name_plural = 'Əməkdaşlar'
+    
