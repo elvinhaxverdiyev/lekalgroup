48492f33aa1573ab963ee7f161584fea735ebd93
Əbil Abilli <ebilebilli3@gmail.com>
2025-08-01 03:50:49 +0400

    integrate Nginx into project

--- lekalgroup/lekalgroup/settings.py
+++ lekalgroup/lekalgroup/settings.py
@@ -128,14 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [ BASE_DIR / "static" ]  
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 
 # Default primary key field type
